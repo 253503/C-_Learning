24ca3eb04ead1597d78508575c77f19bbc17fdee
253503 <gayathri.mohandoss@ust.com>
2024-09-06 20:37:48 +0530

    BoxAPI
    BOXAPI

---
+++ BoxAPI-Day19/BoxAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,90 @@
+using Box.V2;
+using Box.V2.Config;
+using Box.V2.JWTAuth;
+using Box.V2.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq.Expressions;
+
+namespace BoxAPIDemo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public async Task<List<BoxItem>> Get()
+        {
+            //try
+            //{
+            string clientId = "uzc2qa40kji5fz7wykcto14f0e3tanv9";
+            string clientSecret = "woQei6eLHnDnuBOxJf6ayStd064mOKCC";
+            string developerToken = "DnS3BR1rYDd2VqGpT11QKsxha6jNHBIS";
+
+            var boxConfig = new BoxConfig(clientId, clientSecret, new Uri("http://localhost"));
+            var boxJWTAuth = new BoxJWTAuth(boxConfig);
+
+            var boxClient = boxJWTAuth.AdminClient(developerToken);
+
+
+            var user = await boxClient.UsersManager.GetCurrentUserInformationAsync();
+
+            var items = await boxClient.FoldersManager.GetFolderItemsAsync("0", 100);
+
+
+            //foreach (var item in items.Entries)
+            //{
+
+            //}
+            //return user.Name;
+            return items.Entries;
+            //}
+            //catch (Exception ex)
+            //{
+            //    return new List<dynamic>(); ;
+            //}
+
+        }
+
+
+        [HttpPost(Name = "uploadImage")]
+        public async Task<string> Post()
+        {
+            //try
+            //{
+            string clientId = "uzc2qa40kji5fz7wykcto14f0e3tanv9";
+            string clientSecret = "woQei6eLHnDnuBOxJf6ayStd064mOKCC";
+            string developerToken = "anC6mCcct4hPwqY7K2nKQXxrr5QPhtTM";
+            var boxConfig = new BoxConfig(clientId, clientSecret, new Uri("http://localhost"));
+            var boxJWTAuth = new BoxJWTAuth(boxConfig);
+            var boxClient = boxJWTAuth.AdminClient(developerToken);
+            using (var stream = new FileStream("Dummy.txt", FileMode.Open))
+            {
+                var fileRequest = new BoxFileRequest
+                {
+                    Name = Path.GetFileName("Dummy.txt"),
+                    Parent = new BoxRequestEntity { Id = "0", Type = BoxType.file },
+                };
+
+                var uplaodedFile = await boxClient.FilesManager.UploadAsync(fileRequest, stream);
+            }
+
+
+
+            return "file uploaded";
+        }
+
+
+    }
+}
